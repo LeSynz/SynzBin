4b45a28969dd4489cf97d14d7b999839d5b03671
LeSynz <bigman@synz.xyz>
2025-11-15 19:40:44 +0000

    Basic routing added. Initial landing page done.

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+function loadRoutes(dir, baseRoute = '') {
+    fs.readdirSync(dir).forEach(file => {
+        const fullPath = path.join(dir, file);
+        const stat = fs.statSync(fullPath);
+
+        if (stat.isDirectory()) {
+            loadRoutes(fullPath, path.join(baseRoute, file));
+        } else if (file.endsWith('.js')) {
+            const route = require(fullPath);
+            let routePath;
+            if (file === 'index.js') {
+                routePath = baseRoute || '/';
+            } else {
+                routePath = path.join(baseRoute, file.replace('.js', ''));
+            }
+            app.use(routePath, route);
+        }
+    });
+}
+
+loadRoutes(path.join(__dirname, 'routes'));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
